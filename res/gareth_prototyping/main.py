f27234d81e29041413c2d8f18ca8617eae07c70c
Gareth Hogan <gareth.hogan22@mail.dcu.ie>
2024-01-28 11:13:57 +0000

    Changed the training section to use a dictionary, also added an unknown option for when the bot doesn't know how to respond, removed the name entering, and overall added more data to training set so Minerva can answer the most basic of queries about what she is and what she can do
    Signed-off-by: Gareth Hogan <gareth.hogan22@mail.dcu.ie>

--- res/gareth_prototyping/main.py
+++ res/gareth_prototyping/main.py
@@ -1,26 +1,37 @@
 from chatterbot import ChatBot
 from chatterbot.trainers import ListTrainer
+from training_dict import trainset
 
 # Create a new instance of a ChatBot
-bot = ChatBot('Minerva')
+bot = ChatBot('Minerva',
+    logic_adapters=[
+            {
+                'import_path': 'chatterbot.logic.BestMatch',
+                'default_response': "I'm sorry but I don't understand your message, I am still learning. If you don't know what to ask just ask me “What can you do?” and I will be able to show you!",
+                'maximum_similarity_threshold': 0.90
+            }
+        ]
+    )
+
+# empty storage, reset the responses that might be built in, DO NOT RUN EVERYTIME
+# bot.storage.drop()
 
 trainer = ListTrainer(bot)
 trainer.train("chatterbot.corpus.english")
 
-with open("training_data.txt", "r") as f:
-    while True:
-        query = f.readline().strip()
-        resp = f.readline().strip()
-        if not resp: break
+# Training with the trainset
+for resp in trainset:
+    query_list = trainset[resp]
+    for q in query_list:
+        trainer.train([q, resp])
 
-        trainer.train([query, resp])
-
-name=input("Enter Your Name: ")
+# Welcome Message
+print("\033[95m Welcome to Minerva, start chatting with her below! \033[0m")
 
 # The following loop will execute each time the user enters input
 while True:
     try:
-        user_input = input(name + ": ")
+        user_input = input("User" + ": ")
 
         print(f"Minerva: {bot.get_response(user_input)}")
 
