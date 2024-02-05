54703e71b3b27b2ac710a3a200a7ff2705cf717c
Gareth Hogan <gareth.hogan22@mail.dcu.ie>
2024-02-05 15:50:09 +0000

    Cleaned up testing script and how it works in main.py by adding various flags that turn on/off things like training and testing
    Signed-off-by: Gareth Hogan <gareth.hogan22@mail.dcu.ie>

--- res/gareth_prototyping/main.py
+++ res/gareth_prototyping/main.py
@@ -1,7 +1,13 @@
 from chatterbot import ChatBot
+from chatterbot import comparisons
+from chatterbot import response_selection
 from chatterbot.trainers import ListTrainer
 from training_dict import trainset
 import sys
+import logging
+
+if "log" in sys.argv:
+    logging.basicConfig(level=logging.INFO)
 
 n = len(sys.argv)
 
@@ -11,15 +17,16 @@ bot = ChatBot('Minerva',
             {
                 'import_path': 'chatterbot.logic.BestMatch',
                 'default_response': "I'm sorry but I don't understand your message, I am still learning. If you don't know what to ask just ask me “What can you do?” and I will be able to show you!",
-                'maximum_similarity_threshold': 0.75
+                'maximum_similarity_threshold': 0.90
             }
-        ]
+        ],
+    filters=[],
     )
 
 # empty storage, reset the responses that might be built in, DO NOT RUN EVERYTIME
 # bot.storage.drop()
 
-if n == 1:
+if "train" in sys.argv:
     trainer = ListTrainer(bot)
     # trainer.train("chatterbot.corpus.english")
     # Training with the trainset
@@ -27,34 +34,38 @@ if n == 1:
         query_list = trainset[resp]
         for q in query_list:
                 trainer.train([q, resp])
+                trainer.train([q.lower(), resp])
+                trainer.train([q.upper(), resp])
+                trainer.train([q.title(), resp])
+
 
 # Welcome Message
 print("\033[95m Welcome to Minerva, start chatting with her below! \033[0m")
 
 
-## if no arguments, accept user input from terminal
-if n == 1:
+## user specified
+if "user" in sys.argv:
     # The following loop will execute each time the user enters input
     while True:
         try:
             user_input = input("User" + ": ")
+            resp = bot.get_response(user_input)
 
-            print(f"Minerva: {bot.get_response(user_input)}")
+            print(f"Minerva: {resp} (Conf: {resp.confidence})")
 
         # Press ctrl-c or ctrl-d on the keyboard to exit
         except (KeyboardInterrupt, EOFError, SystemExit):
             break
 # otherwise read in test file
 else:
-    print("Tests Started")
-    test_file = sys.argv[1]
-    with open(test_file, "r") as f:
-        for line in f:
-            query, resp = line.split(":")
-            bot_resp = bot.get_response(query)
-            print(query)
-            print(bot_resp, "&", resp)
-            if bot_resp == resp:
-                print("SUCCESS")
-            else:
-                print("FAIL")
+    for arg in sys.argv[1:]:
+        if arg not in ["train", "log"]:
+            print("Test File Started: ", arg)
+            test_file = sys.argv[1]
+            with open(test_file, "r") as f:
+                for line in f:
+                    query, resp = line.split(":")
+                    bot_resp = bot.get_response(query)
+                    print("Query Given: ", query)
+                    print("Bot Response: ", bot_resp)
+                    print("Expected Response: ", resp)
--- res/gareth_prototyping/testing/run_prompt_tests.sh
+++ res/gareth_prototyping/testing/run_prompt_tests.sh
@@ -2,10 +2,5 @@
 
 echo "Shell Script GO"
 
-# not functional yet
-# if python3 main.py testfile.txt | grep -q 'FAIL'
-# then
-#   echo "Tests Failed"
-# else
-#   echo "Tests Succeded"
-# fi
+cd ../
+python3 main.py testing/testfile.txt
