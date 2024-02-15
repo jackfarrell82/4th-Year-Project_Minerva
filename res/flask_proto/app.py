e94bbe6a15cf6d92c332de6f1cb9e4997f81c7be
Gareth Hogan <gareth.hogan22@mail.dcu.ie>
2024-02-15 18:39:18 +0000

    Changed static path in flask app
    Signed-off-by: Gareth Hogan <gareth.hogan22@mail.dcu.ie>

--- res/flask_proto/app.py
+++ res/flask_proto/app.py
@@ -6,7 +6,7 @@ from chatterbot import filters
 import sys
 import logging
 
-app = Flask(__name__)
+app = Flask(__name__, static_url_path='', static_folder="./static")
 
 #### CHATBOT ##########################
 
