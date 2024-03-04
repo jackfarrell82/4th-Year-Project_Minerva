0c79bdb9b74cdafb3b76710880bc7526bb3f8649
jack farrell <jack.farrell82@mail.dcu.ie>
2024-03-04 19:19:48 +0000

    this commit is coming from the laptop, added coments to the code to highlight what is happening and what parts need to change based on the system

--- src/text-to-sql-api.py
+++ src/text-to-sql-api.py
@@ -3,37 +3,36 @@ import mysql.connector
 from mysql.connector import errorcode
 
 config = {
-  'user': 'DESKTOP-7LEP5EQ',
+  'user': 'LAPTOP-EDUNB10Q', #Device Name
   'password': 'Destiny14',
-  'host': '192.168.0.150', #192.168.0.150
+  'host': '192.168.0.65', #Device ipv4 address
   'port': '3306',
   'database': 'financial',
   'raise_on_warnings': True,
 }
 
-
-prompt = "how many iterations do we have where open is less than close"
+prompt = "how many iterations do we have where open is less than close" # Will change this to arguments
 
 headers = {
-    'Authorization': 'Bearer 59ad71fc36bd590f4237955b6ac578d612c5cad6825dcf908aa8e76d0af1b01c',
+    'Authorization': 'Bearer 59ad71fc36bd590f4237955b6ac578d612c5cad6825dcf908aa8e76d0af1b01c', # what allows us to use the api
 }
 
 data = {
-    "prompt": prompt,
-    "type": "mysql",
+    "prompt": prompt, # passes in the prompt
+    "type": "mysql", # the language it is translating too
     "schema": "indexprocessed (Index: text, Date: text, Open: double, High: double, Low: double, Close: double, Adj Close: double, Volume: double, CloseUSD: double)"
 }
 
-response = requests.get("https://www.text2sql.ai/api/sql/generate", headers=headers, data=data)
-response = response.text
-list = response.split('"')
+response = requests.get("https://www.text2sql.ai/api/sql/generate", headers=headers, data=data) # send it to the api
+response = response.text # get the text version back
+list = response.split('"') # modify the output to be just the SQL query
 response = list[3]
 response = response.replace("\\n", " ")
 
 print(prompt)
 print(response)
 
-try:
+try: # error handling for database
     cnx = mysql.connector.connect(**config, auth_plugin='mysql_native_password')
 except mysql.connector.Error as err:
     if err.errno == errorcode.ER_ACCESS_DENIED_ERROR:
@@ -45,8 +44,8 @@ except mysql.connector.Error as err:
 else:
     cursor = cnx.cursor(buffered=True)
 
-    result = cursor.execute(response)
-    rows = cursor.fetchall()    # get all selected rows, as Barmar mentioned
+    result = cursor.execute(response) # sends the query to the database
+    rows = cursor.fetchall()    # gets all the results and prints it out
     for r in rows:
         print(r)
 
